cdf27af44f98f8db3695400173dcb65ede804f57
domzalex <alexdomzalski@gmail.com>
2021-07-09 21:36:11 -0400

    modded about styling, some slight style changes overall

--- src/about.js
+++ src/about.js
@@ -5,20 +5,36 @@ function About() {
       <section className="non-home-section about-section">
         <div className="about-container">
           <div className="about-card">
-            <img src="https://i.ibb.co/SKSsCtF/bfg-banner-1.jpg" alt=""></img>
-            <p>Our classic rock and funk lovechild. Imagine <i>The Meters</i> meets <i>The Allman Brothers</i>.</p>
+            <div className="about-card-title">
+              <h2>The B.F.G. Band</h2>
+            </div>
+            <div className="about-card-info">
+              <p>Our classic rock and funk lovechild. Imagine <i>The Meters</i> meets <i>The Allman Brothers</i>.</p>
+            </div>
           </div>
           <div className="about-card">
-            <img src="https://i.ibb.co/N11jxqc/bfg-lite-1.jpg" alt=""></img>
-            <p>An acoustic arrangement of our full <i>The B.F.G. Band</i>, for a more relaxed and intimate experience.</p>
+            <div className="about-card-title">
+              <h2>B.F.G. Lite</h2>
+            </div>
+            <div className="about-card-info">
+              <p>An acoustic arrangement of our full <i>The B.F.G. Band</i>, for a more relaxed and intimate experience.</p>
+            </div>
           </div>
           <div className="about-card">
-            <img src="https://i.ibb.co/HCvWZGN/fis-banner.jpg" alt=""></img>
-            <p>Our <i>Grateful Dead</i> tribute group focused on high-energy, funked out cover songs.</p>
+            <div className="about-card-title">
+              <h2>Funk is Dead</h2>
+            </div>
+            <div className="about-card-info">
+              <p>Our <i>Grateful Dead</i> tribute group focused on high-energy, funked out cover songs.</p>
+            </div>
           </div>
           <div className="about-card">
-            <img src="https://i.ibb.co/px1fRgR/dj-banner.jpg" alt=""></img>
-            <p>Professional DJ services with 20 years of experience, including a state-of-the-art PA and light show.</p>
+            <div className="about-card-title">
+              <h2>Pro DJ Services</h2>
+            </div>
+            <div className="about-card-info">
+              <p>Professional DJ services with 20 years of experience, including a state-of-the-art PA and light show.</p>
+            </div>
           </div>
         </div>
       </section>
